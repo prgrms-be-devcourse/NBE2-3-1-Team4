e7905a057401206845fe75c434992763c6c4f4da
hyeromon <trina9018@gmail.com>
2024-12-04 16:56:33 +0900

    상품메뉴 불러오기 및 주문상품 수량 불러오기

---
+++ src/main/java/com/example/cafe/dao/UpdateItemDAO.java
@@ -0,0 +1,28 @@
+package com.example.cafe.dao;
+
+import com.example.cafe.dto.ItemTO;
+import com.example.cafe.dto.OrderItemTO;
+import com.example.cafe.dto.OrdersTO;
+import com.example.cafe.mapper.UpdateItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UpdateItemDAO {
+    @Autowired
+    private UpdateItemMapper updateItemMapper;
+
+    public List<ItemTO> getAllItems() {
+        return updateItemMapper.findAllItems();
+    }
+    //해당 이메일로 주문 내역 조회
+    public List<OrderItemTO> getOrderByEmail(String email) {
+        return updateItemMapper.findOrdersByEmail(email);
+    }
+
+   // public OrdersTO getUserByEmail(String email) {
+//        return updateItemMapper.findUserByEmail(email);
+//    }
+}
---
+++ src/main/java/com/example/cafe/mapper/UpdateItemMapper.java
@@ -0,0 +1,20 @@
+package com.example.cafe.mapper;
+
+import com.example.cafe.dto.ItemTO;
+import com.example.cafe.dto.OrderItemTO;
+import com.example.cafe.dto.OrdersTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UpdateItemMapper {
+    // 이메일로 해당 주문 내역 조회
+    List<OrderItemTO> findOrdersByEmail(String email);
+
+    // 이메일로 사용자 정보 조회
+    //OrdersTO findUserByEmail(String email);
+
+    // 상품 목록 조회
+    List<ItemTO> findAllItems();
+}
